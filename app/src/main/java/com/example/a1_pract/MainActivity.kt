7fd763dade6ddbec54db7e3d5f9f05a9d4cc69d6
Solomka22 <Sardorkom954@gmail.com>
2024-11-08 11:06:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a1_pract/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.a1_pract
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn: Button = findViewById(R.id.btn_1)
+        val nameuser : EditText = findViewById(R.id.editTextText)
+
+        btn.setOnClickListener {
+            val inputTxt = nameuser.text.toString()
+            intent = Intent(this,MainActivity2::class.java)
+            intent.putExtra("text", "привет, $inputTxt"  )
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a1_pract/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.a1_pract
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import org.w3c.dom.Text
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val text : TextView = findViewById(R.id.textView)
+        val massage = intent.getStringExtra("text")
+
+        text.text = massage
+    }
+
+}
